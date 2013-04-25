d2618f085278bf5c089e80866b2c4a0f7f531516
Stephen Johnston <stephen.j.johnston@gmail.com>
2013-04-24 22:28:30 -0500

    cleaned up code a little.  Broke larger functions into smaller ones.

--- src/clj_sked/core.clj
+++ src/clj_sked/core.clj
@@ -2,16 +2,20 @@
 
 (defn create-scheduler []
   { :next-fire-map (ref (sorted-map))
-   :name-map (ref (sorted-map))
-   :background-future (ref nil)
-   })
+    :name-map (ref (sorted-map))
+    :background-future (ref nil)
+  } )
+
+(defn- get-fire-map-first-entry [fire-map-first-entry]
+  (if (nil? fire-map-first-entry)
+    nil
+    (second fire-map-first-entry)))
 
 (defn get-next-fire-time [scheduler]
   (let [next-fire-map (:next-fire-map scheduler)]
-    (if (or ( nil? @next-fire-map) (empty? @next-fire-map)) nil
-        (let [lst (first @next-fire-map)]
-          (if (nil? lst) nil
-              (second lst))))))
+    (if (empty? @next-fire-map)
+      nil
+      (get-fire-map-first-entry (first @next-fire-map)))))
 
 (defn remove-job [scheduler item-name]
   (let [next-fire-map (:next-fire-map scheduler)
@@ -22,33 +26,32 @@
      (alter name-map dissoc item-name))
     target-item))
 
+(defn- sleep-until-fire-time [next-fire-time]
+  (let [current-time (System/currentTimeMillis)
+        sleep-time (- next-fire-time current-time)]
+    (when ( > sleep-time 0)
+      (Thread/sleep sleep-time))))
 
-(defn loop-forever [scheduler]
-  (loop [done false]
-    ;; if we are done or if this thread has been interrupted, stop looping
+(defn- service-next-job [scheduler next-items-lst]
+  (sleep-until-fire-time (:fire-time (first next-items-lst)))
+  (doseq [item next-items-lst]
+    ;;(prn "*** firing and removing job for " (:name item))
+    (remove-job scheduler (:name item))))
 
-    (if (or done (.isInterrupted (Thread/currentThread))) (do (prn "scheduler thread is exiting") nil)
-        (let [next-items-lst (get-next-fire-time scheduler)]
-
-          (if (nil? next-items-lst)
-            (recur true)
-            (let [current-time (System/currentTimeMillis)
-                  next-fire-time (:fire-time (first next-items-lst))
-                  sleep-time (- next-fire-time current-time)]
-              (if (> sleep-time 0)
-                (do
-                  (prn "sleeping for " sleep-time)
-                  (Thread/sleep sleep-time))
-                (prn "missed the fire time by: " (- current-time next-fire-time) "ms"))
-
-              (doseq [item next-items-lst]
-                ;;(prn "*** firing and removing job for " (:name item))
-                (remove-job scheduler (:name item)))
-              (recur false)))))))
+(defn service-all-scheduled-jobs [scheduler]
+  (loop []
+    (if (.isInterrupted (Thread/currentThread)) ;; if this thread has been interrupted, return nil (and stop looping)
+      nil
+      (let [next-items-lst (get-next-fire-time scheduler)]
+        (if (nil? next-items-lst) ;; if there is no task to fire, return nil (and stop looping)
+          nil
+          (do ;; process the next job
+            (service-next-job scheduler next-items-lst)
+            (recur)))))))
 
 (defn start-scheduler [scheduler]
   (let [future-ref (:background-future scheduler)
-        new-future (future (loop-forever scheduler))]
+        new-future (future (service-all-scheduled-jobs scheduler))]
     (dosync (ref-set future-ref new-future))))
 
 (defn cancel-future? [scheduler item]
