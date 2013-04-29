c73188d6f1b83317d9cda65648985d1c9a3ef668
Stephen Johnston <stephen.j.johnston@gmail.com>
2013-04-28 20:03:30 -0500

    Added a few tests to get the test-suite started.

--- test/clj_sked/core_test.clj
+++ test/clj_sked/core_test.clj
@@ -2,6 +2,23 @@
   (:use clojure.test
         clj-sked.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest creating-scheduler-test
+  (testing "Test creating an empty scheduler"
+    (let [sked (create-scheduler)]
+      (is (= {}  @(:next-fire-map sked)))
+      (is (= {} @(:name-map sked)))
+      (is (= nil @(:background-future sked))))))
+
+(deftest insert-job-test
+  (testing "Test creating an empty scheduler"
+    (let [sked (create-scheduler)
+          testjob {:fire-time (+ 5000 (System/currentTimeMillis)),
+                   :name "testjob" }
+          dmy   (insert-job sked testjob)]
+      (is (= 1 (count @(:next-fire-map sked))))
+      (is (= 1 (count @(:name-map sked))))
+      (is (= (future? @(:background-future sked))))
+;;      (is (= 0 (count @(:next-fire-map sked))))
+;;      (is (= 0 (count @(:name-map sked))))
+;;      (is (= (future-done? @(:background-future sked))))
+      )))
